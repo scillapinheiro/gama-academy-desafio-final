c0616d99bfd73aa1f9cc4d06a6a839f71197c018
Priscilla Pinheiro <priscilla_pinheiro1@hotmail.com>
2021-12-07 11:35:32 -0300

    Primeiro commit

---
+++ src/pages/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <p>Navbar</p>
+        );
+    }
+}
+
+export default Navbar;
---
+++ src/pages/PageLanding.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PageLanding extends React.Component {
+    render() {
+        return (
+            <p>Page Landing</p>
+        );
+    }
+}
+
+export default PageLanding;
